ae33e648487a2ecadadb922772ad5ddb0fa2276e
João Ronaldo Del Ducca Cunha <joao.ronaldo@gmail.com>
2016-09-04 11:46:07 -0300

    Adicionado launch.json para depuração de código.

---
+++ script.js
@@ -0,0 +1,12 @@
+function soma(a, b)
+{
+    var resultado = a + b;
+    return resultado;
+}
+
+var entrada1 = 10;
+var entrada2 = 5;
+
+var calculo = soma(entrada1, entrada2);
+
+console.log(calculo);
